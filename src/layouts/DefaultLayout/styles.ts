ee9781cc93d025bbb86a7c42ca54438ee0fa1c2e
hevelinlima <hevelin25@gmail.com>
2024-05-07 23:13:16 -0300

    centering the main section

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
   max-width: 90rem;
-  margin: 1.5rem 4rem;
+  margin: 1.5rem auto;
   padding: 0.5rem 4rem;
 
   display: flex;
